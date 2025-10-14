2bc8c29bde00722ad3f7c76d62f6ea6ddf72a138
rakeshranjandas <rakeshranjandas.bbsr93@gmail.com>
2025-10-15 01:51:33 +0530

    SQL schema

---
+++ cp-prep.sql
@@ -0,0 +1,275 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Oct 15, 2025 at 01:50 AM
+-- Server version: 8.0.43-0ubuntu0.22.04.2
+-- PHP Version: 8.1.2-1ubuntu2.22
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cp-prep`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `anime`
+--
+
+CREATE TABLE `anime` (
+  `id` int NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `status` enum('ADDED','WATCHING','FINISHED') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT 'ADDED',
+  `started_watching` datetime DEFAULT NULL,
+  `finished_watching` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sessions`
+--
+
+CREATE TABLE `sessions` (
+  `id` int NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `description` text,
+  `status` enum('IN PROGRESS','COMPLETED','PENDING') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT 'PENDING',
+  `due_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `completed_date` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sessions_task_occurences`
+--
+
+CREATE TABLE `sessions_task_occurences` (
+  `id` int NOT NULL,
+  `sessions_id` int NOT NULL,
+  `task_occurences_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tags`
+--
+
+CREATE TABLE `tags` (
+  `id` int NOT NULL,
+  `name` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tasks`
+--
+
+CREATE TABLE `tasks` (
+  `id` int NOT NULL,
+  `title` varchar(200) NOT NULL,
+  `description` text,
+  `url` varchar(500) DEFAULT NULL,
+  `platform` varchar(200) DEFAULT NULL,
+  `added_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tasks_repeat`
+--
+
+CREATE TABLE `tasks_repeat` (
+  `id` int NOT NULL,
+  `tasks_id` int NOT NULL,
+  `repeat_policy` varchar(200) NOT NULL,
+  `last_task_occurences_id` int NOT NULL,
+  `last_task_occurences_count` int NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tasks_tags`
+--
+
+CREATE TABLE `tasks_tags` (
+  `id` int NOT NULL,
+  `tasks_id` int NOT NULL,
+  `tags_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `task_occurences`
+--
+
+CREATE TABLE `task_occurences` (
+  `id` int NOT NULL,
+  `tasks_id` int NOT NULL,
+  `status` enum('IN PROGRESS','PENDING','COMPLETED','') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT 'PENDING',
+  `added_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `due_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `completed_date` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `anime`
+--
+ALTER TABLE `anime`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `sessions`
+--
+ALTER TABLE `sessions`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `sessions_task_occurences`
+--
+ALTER TABLE `sessions_task_occurences`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `sessions_id` (`sessions_id`),
+  ADD KEY `task_occurences_id` (`task_occurences_id`);
+
+--
+-- Indexes for table `tags`
+--
+ALTER TABLE `tags`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`);
+
+--
+-- Indexes for table `tasks`
+--
+ALTER TABLE `tasks`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `url` (`url`),
+  ADD KEY `title` (`title`(5));
+
+--
+-- Indexes for table `tasks_repeat`
+--
+ALTER TABLE `tasks_repeat`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `tasks_id` (`tasks_id`),
+  ADD KEY `last_task_occurences_id` (`last_task_occurences_id`);
+
+--
+-- Indexes for table `tasks_tags`
+--
+ALTER TABLE `tasks_tags`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `tasks_id` (`tasks_id`),
+  ADD KEY `tags_id` (`tags_id`);
+
+--
+-- Indexes for table `task_occurences`
+--
+ALTER TABLE `task_occurences`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `tasks_id` (`tasks_id`),
+  ADD KEY `due_date` (`due_date`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `anime`
+--
+ALTER TABLE `anime`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `sessions`
+--
+ALTER TABLE `sessions`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `sessions_task_occurences`
+--
+ALTER TABLE `sessions_task_occurences`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tags`
+--
+ALTER TABLE `tags`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tasks`
+--
+ALTER TABLE `tasks`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tasks_repeat`
+--
+ALTER TABLE `tasks_repeat`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tasks_tags`
+--
+ALTER TABLE `tasks_tags`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `task_occurences`
+--
+ALTER TABLE `task_occurences`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `sessions_task_occurences`
+--
+ALTER TABLE `sessions_task_occurences`
+  ADD CONSTRAINT `sessions_task_occurences_ibfk_1` FOREIGN KEY (`sessions_id`) REFERENCES `sessions` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `sessions_task_occurences_ibfk_2` FOREIGN KEY (`task_occurences_id`) REFERENCES `task_occurences` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `tasks_repeat`
+--
+ALTER TABLE `tasks_repeat`
+  ADD CONSTRAINT `tasks_repeat_ibfk_1` FOREIGN KEY (`tasks_id`) REFERENCES `tasks` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `tasks_repeat_ibfk_2` FOREIGN KEY (`last_task_occurences_id`) REFERENCES `task_occurences` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `tasks_tags`
+--
+ALTER TABLE `tasks_tags`
+  ADD CONSTRAINT `tasks_tags_ibfk_1` FOREIGN KEY (`tags_id`) REFERENCES `tags` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `tasks_tags_ibfk_2` FOREIGN KEY (`tasks_id`) REFERENCES `tasks` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
